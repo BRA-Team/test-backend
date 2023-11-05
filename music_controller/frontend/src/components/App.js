fc17a71bb2ea74ef6c37e12ed769e43dd976b74e
Bogdan_Iam <Iamnitchi.Io.Bogdan@student.utcluj.ro>
2023-11-05 12:35:32 +0200

    resolved #9

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import HomePage from "./HomePage";
 import {
     BrowserRouter as Router,
@@ -22,8 +22,8 @@ export default class App extends Component {
         <Router>
             <Routes>
                 <Route path="" element={<HomePage />}/>
-                <Route path="join/" element={<RoomJoinPage />}/>
-                <Route path="create/" element={<CreateRoomPage />}/>
+                <Route path="join" element={<RoomJoinPage />}/>
+                <Route path="create" element={<CreateRoomPage />}/>
             </Routes>
         </Router>
     );
@@ -31,7 +31,5 @@ export default class App extends Component {
 }
 
 const appDiv = document.getElementById("app");
-render(
-    <App />
-    ,appDiv
-);
+const root = createRoot(appDiv);
+root.render(<App />);
